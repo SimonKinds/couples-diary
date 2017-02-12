1ebee39ce883598f8e16608e06e0c6b8d93adb12
Simon Kindström <simon.kinds@gmail.com>
2017-02-12 13:07:01 +0700

    Moved the api to a subdomain and directory

---
+++ api/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+
+// db
+const mongoose = require('mongoose');
+mongoose.connection.on('open', (ref) => console.log('Connected to mongodb'));
+mongoose.connection.on(
+    'error', (ref) => console.log('Error connecting to mongodb'));
+mongoose.connect('mongodb://localhost/couples_diary');
+
+// parse request bodies with json
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+// set up API routes
+const diary = require('./routes/diary');
+app.use('/diary', diary);
+const user = require('./routes/user');
+app.use('/user', user);
+const login = require('./routes/login');
+app.use('/login', login);
+
+module.exports = app;
--- index.js
+++ index.js
@@ -1,36 +1,9 @@
 const express = require('express');
+const vhost = require('vhost');
+
 const app = express();
+const api = require('./api/index');
 
-// db
-const mongoose = require('mongoose');
-mongoose.connection.on('open', (ref) => console.log('Connected to mongodb'));
-mongoose.connection.on(
-    'error', (ref) => console.log('Error connecting to mongodb'));
-mongoose.connect('mongodb://localhost/couples_diary');
-
-// set up Express
-// enable sessions
-const session = require('express-session');
-const MongoStore = require('connect-mongo')(session);
-
-// TODO: update the secret
-app.use(session({
-  secret: 'key to sign the cookie',
-  store: new MongoStore({mongooseConnection: mongoose.connection}),
-  resave: false,            // don't write to db if no changes have been made
-  saveUninitialized: false  // don't save new but non-existing sessions
-}));
-
-// parse request bodies with json
-const bodyParser = require('body-parser');
-app.use(bodyParser.json());
-
-// set up API routes
-const diary = require('./routes/diary');
-app.use('/diary', diary);
-const user = require('./routes/user');
-app.use('/user', user);
-const login = require('./routes/login');
-app.use('/login', login);
+app.use(vhost('api.localhost', api));
 
 app.listen(8080, () => {console.log('Listening on port 8080')});
