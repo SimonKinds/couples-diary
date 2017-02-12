4d9dac0221d1e3fcbfbdb71732ab2ceef7cf6c4f
Simon Kindström <simon.kinds@gmail.com>
2017-02-12 15:56:06 +0700

    Updated create user API

--- api/models/user.js
+++ api/models/user.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 
-const UserSchema = new mongoose.Schema(
-    {username: String, first_name: String, last_name: String});
+const UserSchema = new mongoose.Schema({username: String});
 
 module.exports = mongoose.model('User', UserSchema);
--- api/routes/user.js
+++ api/routes/user.js
@@ -4,14 +4,28 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const User = require('../models/user.js');
 
-router.post(
-    '/', (request, response) => {User.create(request.body, (err, post) => {
-           if (err) {
-             console.log(err);
-             response.send('Error')
-           } else {
-             response.json(post)
-           }
-         })});
+router.post('/create', (request, response) => {
+  const username = request.body.username;
+  if (!username) {
+    response.status(400).send({reason: 'Missing username'});
+  } else {
+    User.findOne({username: username}, (err, db_response) => {
+      if (err) {
+        response.status(500).send(err);
+      } else if (db_response) {
+        // this username already exists
+        response.status(409).send();
+      } else {
+        User.create({username: username}, (err, post) => {
+          if (err) {
+            response.status(500).send(err);
+          } else {
+            response.status(200).send();
+          }
+        });
+      }
+    })
+  }
+});
 
 module.exports = router;
