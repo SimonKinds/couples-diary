477ed5719863c9ae923ca23fad9230002971398d
Simon Kindström <simon.kinds@gmail.com>
2017-02-13 14:11:06 +0700

    Enabled serving of index.html via localhost

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -3,15 +3,28 @@ import {LocalForm, Control} from 'react-redux-form';
 
 export default class LoginForm extends React.Component {
   render() {
-    return (<LocalForm
-      onSubmit={values => this.handleSubmit(values)}>
-      <label>Username</label>
-      <Control.text model='.username'/>
-    </LocalForm>);
+    return (
+      <div>
+        <LocalForm
+          onSubmit={values => this.handleSubmit(values)}>
+          <label>Username</label>
+          <Control.text model='.username'/>
+        </LocalForm>
+        <button onClick={this.handleSubmit}>
+          Login
+        </button>
+        <button onClick={this.routeToSignup}>
+          Sign Up
+        </button>
+      </div>
+    );
   }
-
 
   handleSubmit(values) {
     alert(values.username);
+  }
+
+  routeToSignup() {
+    alert('I want to sign up');
   }
 };
--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const vhost = require('vhost');
+const path = require('path');
 
 const app = express();
 const api = require('./api/index');
 
+app.get(
+    '/', (req, res) => { res.sendFile(path.join(__dirname + '/index.html')); });
+app.get('/bundle.js', (req, res) => {
+  res.sendFile(path.join(__dirname + '/../build/bundle.js'));
+});
 app.use(vhost('api.localhost', api));
 
 app.listen(8080, () => {console.log('Listening on port 8080')});
