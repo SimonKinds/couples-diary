70029d67e14eeee282d91be2c1e38c4544240591
Simon Kindström <simon.kinds@gmail.com>
2017-02-15 19:01:59 +0700

    Removed unused action

--- src/features/login/LoginActions.js
+++ src/features/login/LoginActions.js
@@ -1,7 +1,6 @@
 import fetch from 'node-fetch'
 
 export const LOGINFORM_ON_CHANGE = 'LOGINFORM_ON_CHANGE'
-export const LOGINFORM_SUBMIT = 'LOGINFORM_SUBMIT'
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS'
 export const LOGIN_FAIL = 'LOGIN_FAIL'
 
@@ -9,8 +8,7 @@ export function loginFormOnChange(username) {
   return { type: LOGINFORM_ON_CHANGE, username: username }
 }
 
-function
-loginFormSubmit() {
+function loginFormSubmit() {
   return { type: LOGINFORM_SUBMIT }
 }
 
@@ -18,15 +16,12 @@ function loginSuccess(token) {
   return { type: LOGIN_SUCCESS, token: token }
 }
 
-function
-loginFail() {
+function loginFail() {
   return { type: LOGIN_FAIL }
 }
 
 export function login(username) {
   return dispatch => {
-    dispatch(loginFormSubmit())
-
     return fetch('http://api.localhost:8080/login',
       { method: 'POST',
         headers: {'Content-Type': 'application/json'},
