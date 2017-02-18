bc3ca7ec94906e3c8e851a899cc4a9b10727aefe
Simon Kindström <simon.kinds@gmail.com>
2017-02-18 17:33:17 +0800

    Added onClick listener to Date component

--- src/features/date/Date.jsx
+++ src/features/date/Date.jsx
@@ -2,14 +2,17 @@ import React, {PropTypes} from 'react'
 
 const Date = (props) => {
   return (
-    <div style={{flex: 1, border: '2px solid black', borderRadius: '5px'}}>
+    <div style={
+      {flex: 1, border: '2px solid black', borderRadius: '5px'}}
+      onClick={props.onClick}>
       {props.day}
     </div>
   )
 }
 
 Date.propTypes = {
-  day: PropTypes.number.isRequired
+  day: PropTypes.number.isRequired,
+  onClick: PropTypes.func.isRequired
 }
 
 export default Date
