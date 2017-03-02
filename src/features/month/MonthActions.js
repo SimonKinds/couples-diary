86f5f9a201a2f413c17c38bac14444dee092c2b2
Simon Kindström <simon.kinds@gmail.com>
2017-03-02 10:06:36 +0900

    Added dummy actions for fetching month

---
+++ src/features/month/MonthActions.js
@@ -0,0 +1,17 @@
+export const MONTH_GET = "MONTH_GET"
+export const MONTH_FETCHING = "MONTH_FETCHING"
+export const MONTH_GET_SUCCESS = "MONTH_GET_SUCCESS"
+
+export function monthGet(year, month) {
+  return dispatch => {
+    dispatch(monthGetSuccess(year, month, [{day: 1, year: year, month: month,  entries: [{name: 'simon', content: 'Today I\'ve been hiding from the rain', color: '#ff00ff'}]}, {day: 2, year: year, month: month, entries: [{name: 'simon', content: 'Today I saw the puking lion', color: '#ff00ff'}]}, {day: 3, year: year, month: month}, {day: 4, year: year, month: month}]))
+  }
+}
+
+function monthFetching(year, month) {
+  return {type: MONTH_FETCHING, year: year, month: month}
+}
+
+function monthGetSuccess(year, month, days) {
+  return {type: MONTH_GET_SUCCESS, year: year, month: month, days: days}
+}
