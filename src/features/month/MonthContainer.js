f910b00cede9c6804a953034994c5b6b030e7a1b
Simon Kindström <simon.kinds@gmail.com>
2017-03-02 13:01:02 +0900

    Created MonthContainer

---
+++ src/features/month/MonthContainer.js
@@ -0,0 +1,24 @@
+import {connect} from 'react-redux'
+
+import Month from './Month'
+import {monthGet} from './MonthActions'
+
+const mapStateToProps = state => {
+  const date = new Date()
+  date.setYear(state.month.year)
+  date.setMonth(state.month.month)
+  // 0 = last date of previous month
+  date.setDate(1)
+  return {
+    startIndex: date.getDay(),
+    // long = full name
+    monthName: date.toLocaleString('en-us', { month: "long"}),
+    days: state.month.days
+  }
+}
+
+const MonthContainer = connect(
+  mapStateToProps
+)(Month)
+
+export default MonthContainer
