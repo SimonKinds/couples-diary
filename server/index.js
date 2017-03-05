1e170a952a9b7e102a628045d6ba943c7e513ad9
Simon Kindström <simon.kindstrom@gmail.com>
2017-03-05 17:58:11 +0900

    Removed unused libraries

--- server/index.js
+++ server/index.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const vhost = require('vhost')
 const path = require('path')
 
 const app = express()
--- server/src/api/index.js
+++ server/src/api/index.js
@@ -1,17 +1,9 @@
 const express = require('express');
 const app = express();
-const cors = require('cors');
-
-app.use(cors());  // enable CORS across all API calls
-// This might be needed for "complex" CORS calls
-// app.options('*', cors());
 
 // db
 const mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
-mongoose.connection.on('open', (ref) => console.log('Connected to mongodb'));
-mongoose.connection.on(
-    'error', (ref) => console.log('Error connecting to mongodb'));
 mongoose.connect('mongodb://localhost/couples_diary');
 
 // parse request bodies with json
