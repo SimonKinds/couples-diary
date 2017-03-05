24a385a71dbbf8e4548284198a32fc6d90df426e
Simon Kindström <simon.kindstrom@gmail.com>
2017-03-05 16:13:21 +0900

    Moved server into a seperate directory

--- server/index.js
+++ server/index.js
@@ -3,13 +3,13 @@ const vhost = require('vhost')
 const path = require('path')
 
 const app = express()
-const api = require('./api/index')
+const api = require('./src/api/index')
 
 app.use('/api', api)
 
 app.use(express.static('build'))
 
-app.get('/*', (req, res) => res.sendFile(path.join(__dirname, '../public/index.html')))
+app.get('/*', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')))
 
 
 app.listen(8080, () => {console.log('Listening on port 8080')})
