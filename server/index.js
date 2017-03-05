1481dbe8bc41990aaa0d90dcdd14f6117ec2c91d
Simon Kindström <simon.kindstrom@gmail.com>
2017-03-05 21:00:05 +0900

    Reorder server files

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')
 
 const app = express()
-const api = require('./src/api/index')
+const api = require('./src/api')
 
 app.use('/api', api)
 
