72cacdeaba6cc091cfa4b184e01fdb5c990cc1f8
Simon Kindström <simon.kindstrom@gmail.com>
2017-04-04 15:45:28 +0900

    Full size MonthContainer in Diary

--- client/src/components/Diary.jsx
+++ client/src/components/Diary.jsx
@@ -3,18 +3,28 @@ import React from 'react';
 import MonthContainer from '../containers/MonthContainer';
 
 function Diary(props) {
-  return (<div>
-    <b>{props.year}</b>
-    <div style={{display: 'flex', alignItems: 'center'}}>
-      <div onClick={() => props.showPrevMonth(props.year, props.month)}>
-        &lt;
-      </div>
-      <MonthContainer />
-      <div onClick={() => props.showNextMonth(props.year, props.month)}>
-        &gt;
+  return (
+    <div>
+      <b>{props.year}</b>
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <div
+          style={{ flex: 1 }}
+          onClick={() => props.showPrevMonth(props.year, props.month)}
+        >
+          &lt; Previous month
+        </div>
+        <div style={{ flex: '1 100%' }}>
+          <MonthContainer />
+        </div>
+        <div
+          style={{ flex: 1 }}
+          onClick={() => props.showNextMonth(props.year, props.month)}
+        >
+          Next month &gt;
+        </div>
       </div>
     </div>
-  </div>)
+  );
 }
 
 export default Diary;
