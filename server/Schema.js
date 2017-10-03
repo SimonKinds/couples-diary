4906b6fdf280af131239b0d58e2fee80108be73f
Simon Kindström <simon.kindstrom@gmail.com>
2017-10-03 23:19:08 +0200

    Added Couple and Query type to GraphQL scheme

--- server/Schema.js
+++ server/Schema.js
@@ -1,20 +1,68 @@
 // @flow
 
 const typeDefs = [`
+  scalar Date
+
   type Post {
+    id: ID!,
     author: User!,
     content: String!,
-    year: Int!,
-    month: Int!,
-    day: Int!,
-    creation_time: Int!,
-    read_time: Int!
+    # the date that the post is about
+    date: Date!,
+    creationDate: Date!,
+    # the time at which the other user read the post
+    readDate: Date!
   }
 
   type User {
+    id: ID!,
     name: String!,
     username: String!,
     email: String!,
-    creation_time: Int!,
+    creationDate: Date!,
+  }
+
+  type Couple {
+    id: ID!,
+    name: String!,
+    [users]: User!,
+    creationDate: Date!
+  }
+
+  type Query {
+    # returns the couple for the logged-in user
+    couple(): Couple,
+    # the posts by the couple, for the given date
+    postsForDate(date: Date!), [Post],
+    #
+    postsForMonth(year: Int!, month: Int!): [Post]
   }
 `];
+
+const resolvers = {
+  // Scalars
+  Date: new GraphQLScalarType({
+    // http://dev.apollodata.com/tools/graphql-tools/scalars.html#Date-as-a-scalar
+    name: 'Date',
+    description: 'UTC milliseconds since epoch',
+    parseValue(value) {
+      // Turn an input into a date which we want as a number
+      // value from the client
+      return new Date(value).valueOf();
+    },
+    serialize(value) {
+      // Convert Date to number primitive .getTime() or .valueOf()
+      // value sent to the client
+      if (value instanceof Date) return value.valueOf();
+      return value;
+    },
+    parseLiteral(ast) {
+      // ast value is always in string format
+      if (ast.kind === Kind.INT) {
+        // parseInt turns a string number into number of a certain base
+        return parseInt(ast.value, 10);
+      }
+      return null;
+    },
+  }),
+}
