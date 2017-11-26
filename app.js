b59ee8f4946b26eed0f379f5c1cfc7bc59d09262
Simon Kindström <simon.kindstrom@gmail.com>
2017-11-26 16:48:56 +0100

    First commit v2.2
    A server with root path is created

---
+++ app.js
@@ -0,0 +1,7 @@
+// @flow
+import express from 'express';
+import type {$Application, $Request, $Response} from 'express';
+const app: $Application = express();
+
+app.get('/', (req: $Request, res: $Response) => res.status(200).send());
+export default app;
