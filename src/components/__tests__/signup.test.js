684aefd16434da0761e4b7d92133da81b34705ba
Simon Kindström <simon.kindstrom@gmail.com>
2017-11-29 21:18:10 +0100

    Use semantic UI react
    Also add eslint

--- src/components/__tests__/signup.test.js
+++ src/components/__tests__/signup.test.js
@@ -6,9 +6,16 @@ import renderer from 'react-test-renderer';
 import Signup from '../signup.js';
 
 describe('<Signup />', () => {
-  it('matches its snapshot', () => {
+  it('matches its snapshot without error', () => {
     const tree = renderer
-      .create(<Signup onSubmit={() => {return}} />)
+      .create(<Signup onSubmit={() => { return }} error={false} />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('matches its snapshot with error', () => {
+    const tree = renderer
+      .create(<Signup onSubmit={() => { return }} error={true} />)
       .toJSON();
     expect(tree).toMatchSnapshot();
   });
@@ -19,10 +26,10 @@ describe('<Signup />', () => {
       expect(inputEmail).toBe(email);
       done();
     }
-    const wrapper = shallow(<Signup onSubmit={callback}/>);
+    const wrapper = shallow(<Signup onSubmit={callback} error={false} />);
 
-    wrapper.find(`input[name='email']`)
-           .simulate('change', {target: {value: email}});
-    wrapper.find(`button[name='submit']`).simulate('click');
+    wrapper.find('FormInput')
+      .simulate('change', { target: { value: email } });
+    wrapper.find('Button').simulate('click');
   });
 });
--- src/components/signup.js
+++ src/components/signup.js
@@ -1,8 +1,10 @@
 // @flow
 import React from 'react';
+import { Button, Form, Message } from 'semantic-ui-react'
 
 type Props = {
   onSubmit: (string) => void,
+  error: bool,
 };
 
 type State = {
@@ -13,18 +15,26 @@ class SignUp extends React.Component<Props, State> {
   constructor(props: Props) {
     super(props);
 
-    this.state = {email: ''};
+    this.state = { email: '' };
   }
 
   render() {
     return (
-      <form className='signup'>
-      Email:
-      <input name='email' type='text'
-      onChange={e => this.setState({email: e.target.value})}/>
-      <button name='submit' type='submit' value='Submit'
-      onClick={() => this.props.onSubmit(this.state.email)}/>
-      </form>
+      <Form error={this.props.error}>
+        <Form.Input
+          name='email'
+          label='Email'
+          placeholder='john@smith.com'
+          onChange={e => this.setState({ email: e.target.value })} />
+        <Message
+          error
+          header='Error'
+          content='Email already in use' />
+        <Button
+          name='submit'
+          label='Submit'
+          onClick={() => this.props.onSubmit(this.state.email)} />
+      </Form>
     );
   };
 }
