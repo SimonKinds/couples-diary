fbd241e9900aed9f07c8afb3b9758f9911c34fc8
Simon Kindström <simon.kindstrom@gmail.com>
2018-03-13 22:03:14 +0100

    Added common test setup

--- client/App/App.test.js
+++ client/App/App.test.js
@@ -1,10 +1,7 @@
 import React from 'react';
-import { configure, shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import { shallow } from 'enzyme';
 
 import App from './index';
-
-configure({ adapter: new Adapter() });
 
 describe('App component', () => {
   test('Renders a .container', () => {
