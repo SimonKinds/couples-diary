e3566c07089d34bf91ceed90cf92b92118508196
Simon Kindström <simon.kindstrom@gmail.com>
2018-03-15 22:19:17 +0100

    Simple calendar with placeholder entries

--- src/client/Calendar/index.js
+++ src/client/Calendar/index.js
@@ -7,17 +7,27 @@ import './styles.css';
 type Props = {};
 type State = {};
 
+function getDays() {
+  const days = [];
+  for (let i = 0; i < 28; i += 1) {
+    days.push(<div key={i} className="day-entry">{i + 1}</div>);
+  }
+
+  return days;
+}
+
 export default class Calendar extends PureComponent<Props, State> {
   render() {
     return (
       <div className="calendar">
-        <div className="day">Mon</div>
-        <div className="day">Tue</div>
-        <div className="day">Wed</div>
-        <div className="day">Thu</div>
-        <div className="day">Fri</div>
-        <div className="day">Sat</div>
-        <div className="day">Sun</div>
+        <div className="day-name">Mon</div>
+        <div className="day-name">Tue</div>
+        <div className="day-name">Wed</div>
+        <div className="day-name">Thu</div>
+        <div className="day-name">Fri</div>
+        <div className="day-name">Sat</div>
+        <div className="day-name">Sun</div>
+        {getDays()}
       </div>);
   }
 }
