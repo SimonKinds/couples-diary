a583f05db6b9935010522c47f613cfcb4c363a02
Simon Kindström <simon.kindstrom@gmail.com>
2018-03-16 10:11:37 +0100

    Added days of previous / next day in calendar

--- src/client/Calendar/index.js
+++ src/client/Calendar/index.js
@@ -7,11 +7,65 @@ import './styles.css';
 type Props = {};
 type State = {};
 
-function getDays() {
+function getDaysFromPreviousMonth(date: Date) {
+  const startDate = date.getDate();
+  date.setDate(1);
+  const daysFromPreviousMonth = ((date.getDay() + 7) - 1) % 7;
+  date.setDate(startDate);
+
+  const daysInPrevMonth =
+    new Date(date.getFullYear(), date.getMonth(), 0).getDate();
+
+  const prevMonth = date.getMonth() - 1;
+
   const days = [];
-  for (let i = 0; i < 28; i += 1) {
-    days.push(<div key={i} className="day-entry">{i + 1}</div>);
+  for (let i = daysInPrevMonth - daysFromPreviousMonth;
+    i < daysInPrevMonth; i += 1) {
+    days.push(<div key={`M${prevMonth}-D${i}`} className="day-entry not-in-month">{i + 1}</div>);
   }
+
+  return days;
+}
+
+function getDaysFromNextMonth(date: Date) {
+  date.setMonth(date.getMonth() + 1);
+  date.setDate(0);
+
+
+  const daysFromNextMonth = 7 - ((date.getDay() + 7) % 7);
+  const nextMonth = date.getMonth() + 1;
+
+
+  const days = [];
+  for (let i = 0; i < daysFromNextMonth; i += 1) {
+    days.push(<div key={`M${nextMonth}-D${i}`} className="day-entry not-in-month">{i + 1}</div>);
+  }
+
+  return days;
+}
+
+function getDays() {
+  const date = new Date();
+  date.setMonth(date.getMonth() + 1);
+  date.setDate(0);
+
+
+  const daysInMonth = date.getDate();
+
+  date.setDate(1);
+
+  let days = [];
+  days = days.concat(getDaysFromPreviousMonth(date));
+  const daysFromPreviousMonth = days.length;
+
+  for (let i = 0; i < daysInMonth; i += 1) {
+    days.push((
+      <div key={i + daysFromPreviousMonth} className="day-entry">
+        {i + 1}
+      </div>));
+  }
+
+  days = days.concat(getDaysFromNextMonth(date));
 
   return days;
 }
