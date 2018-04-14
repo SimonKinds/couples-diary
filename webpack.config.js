e96adff6bf18823c1d4df9262753ca6307ca5268
Simon Kindström <simon.kindstrom@gmail.com>
2018-04-14 22:38:20 +0200

    Update dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,11 @@ const webpack = require('webpack');
 
 module.exports = {
   entry: ['react-hot-loader/patch', './src/client/index.js'],
+  mode: 'development',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
   },
   module: {
     rules: [
@@ -23,6 +25,7 @@ module.exports = {
   devServer: {
     contentBase: './dist',
     hot: true,
+    historyApiFallback: true,
     proxy: {
       '/api': 'http://127.0.0.1:3000',
     },
