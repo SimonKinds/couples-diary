6f2af966ee5cf00e8b3b1f48ef4e8aa1d354fe33
Simon Kindström <simon.kindstrom@gmail.com>
2018-08-20 23:37:27 +0200

    Show header in 404 page

--- packages/app/src/components/FourOhFour/index.js
+++ packages/app/src/components/FourOhFour/index.js
@@ -1,9 +1,17 @@
-import React, { PureComponent } from 'react';
+import React, { PureComponent, Fragment } from 'react';
+import Header from '../Header';
 
 import './styles.css';
 
 export default class FourOhFour extends PureComponent {
   render() {
-    return <div className="fourohfour">404: No love found</div>;
+    return (
+      <Fragment>
+        <Header />
+        <main>
+          <div className="fourohfour">404: No love found</div>
+        </main>
+      </Fragment>
+    );
   }
 }
