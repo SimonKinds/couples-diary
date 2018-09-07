5e8ad55268aadfc6488dfcfd18ac37067d118e50
Simon Kindström <simon.kindstrom@gmail.com>
2018-09-07 12:01:47 +0200

    Update test script to fail if any test fail

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,11 +4,23 @@ export CI=true
 
 cd packages/server
 yarn test
+
+server_ret=$?
 cd ../../
 
 cd packages/app
 yarn test
+
+app_ret=$?
 cd ../../
 
 cd packages/core
 yarn test
+
+core_ret=$?
+
+if [ $server_ret -ne 0 ] || [ $app_ret -ne 0 ] || [ $core_ret -ne 0 ]; then
+  exit 1
+fi
+
+exit 0
