f53abfcd2b43f1a20053a82b0fed8f349f4774d9
Simon Kindström <simon.kindstrom@gmail.com>
2018-10-08 11:15:43 +0200

    Update test script to not test app
    Because there are no tests there

--- scripts/test.sh
+++ scripts/test.sh
@@ -8,18 +8,12 @@ yarn test
 server_ret=$?
 cd ../../
 
-cd packages/app
-yarn test
-
-app_ret=$?
-cd ../../
-
 cd packages/core
 yarn test
 
 core_ret=$?
 
-if [ $server_ret -ne 0 ] || [ $app_ret -ne 0 ] || [ $core_ret -ne 0 ]; then
+if [ $server_ret -ne 0 ] || [ $core_ret -ne 0 ]; then
   exit 1
 fi
 
