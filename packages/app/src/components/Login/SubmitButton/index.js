5def71895058c9fbd76942f0eb7827650317893a
Simon Kindström <simon.kindstrom@gmail.com>
2018-10-09 09:24:11 +0200

    Fix Login screen crashing

--- packages/app/src/components/Login/SubmitButton/index.js
+++ packages/app/src/components/Login/SubmitButton/index.js
@@ -4,12 +4,12 @@ import PropTypes from 'prop-types';
 import './styles.css';
 
 export default class LoginSubmitButton extends PureComponent {
+  static propTypes = {
+    id: PropTypes.string.isRequired,
+    value: PropTypes.string.isRequired,
+  };
+
   render() {
     return <input id={this.props.id} type="submit" value={this.props.value} />;
   }
 }
-
-LoginSubmitButton = {
-  id: PropTypes.string.isRequired,
-  value: PropTypes.string.isRequired,
-};
