9dd41b2e2e8cfa17b98703b15b418ed1688f5d51
Simon Kindström <simon.kindstrom@gmail.com>
2018-11-29 20:37:53 +0100

    Clean entry rendering code

--- packages/app/src/components/Entry/index.js
+++ packages/app/src/components/Entry/index.js
@@ -34,7 +34,137 @@ const ENTRY_GQL_QUERY = gql`
   }
 `;
 
-const getEntryForAuthor = (author, entries) => {
+function EntryContainer({ year, month, date, author: requestedAuthor }) {
+  return (
+    <Query
+      query={ENTRY_GQL_QUERY}
+      variables={{ year, month, date }}
+      fetchPolicy="cache-and-network"
+    >
+      {renderEntry(year, month, date, requestedAuthor)}
+    </Query>
+  );
+}
+
+function renderEntry(year, month, date, requestedAuthor) {
+  return ({ data: dataFromQuery, loading: loadingQuery }) => (
+    <Entry
+      body={
+        loadingQuery ? (
+          <EntryBody nameOfUser={''} entry={''} loading={loadingQuery} />
+        ) : dataFromQuery.me.name.toLowerCase() ===
+        requestedAuthor.toLowerCase() ? (
+          <EditableEntryBody
+            year={year}
+            month={month}
+            date={date}
+            nameOfUser={
+              (dataFromQuery && dataFromQuery.me && dataFromQuery.me.name) || ''
+            }
+            entry={getEntryForAuthor(
+              requestedAuthor,
+              (dataFromQuery && dataFromQuery.entries) || []
+            )}
+          />
+        ) : (
+          <EntryBody
+            nameOfUser={
+              (dataFromQuery &&
+                dataFromQuery.myCouple &&
+                dataFromQuery.myCouple.partner.name) ||
+              ''
+            }
+            entry={getEntryForAuthor(
+              requestedAuthor,
+              (dataFromQuery && dataFromQuery.entries) || []
+            )}
+            loading={loadingQuery}
+          />
+        )
+      }
+      year={year}
+      month={month}
+      date={date}
+      nameOfPartner={
+        (dataFromQuery && dataFromQuery.myCouple) !== undefined
+          ? dataFromQuery.myCouple.me.name.toLowerCase() ===
+            requestedAuthor.toLowerCase()
+            ? dataFromQuery.myCouple.partner.name
+            : dataFromQuery.myCouple.me.name
+          : ''
+      }
+    />
+  );
+}
+
+function EditableEntryBody({ year, month, date, nameOfUser, entry }) {
+  return (
+    <Mutation
+      mutation={gql`
+        mutation SetEntry(
+          $year: Int!
+          $month: Int!
+          $date: Int!
+          $content: String!
+        ) {
+          setEntry(year: $year, month: $month, date: $date, content: $content) {
+            content
+          }
+        }
+      `}
+      refetchQueries={[
+        { query: CALENDAR_GQL_QUERY, variables: { year, month } },
+        { query: ENTRY_GQL_QUERY, variables: { year, month, date } },
+      ]}
+    >
+      {(
+        setEntry,
+        {
+          data: dataFromMutation,
+          called: didCallMutation,
+          error: errorFromMutation,
+          loading,
+        }
+      ) => {
+        if (didCallMutation && errorFromMutation == null) {
+          removeLocallySavedEntry(
+            locallySavedEntryKey(nameOfUser, year, month, date)
+          );
+        }
+
+        return (
+          <EntryForm
+            saveEntry={content =>
+              setEntry({ variables: { year, month, date, content } })
+            }
+            nameOfUser={nameOfUser}
+            entry={
+              (dataFromMutation &&
+                dataFromMutation.setEntry &&
+                dataFromMutation.setEntry.content) ||
+              entry
+            }
+            getLocallySavedEntry={() =>
+              getLocallySavedEntry(
+                locallySavedEntryKey(nameOfUser, year, month, date)
+              )
+            }
+            locallySaveEntry={entry =>
+              setLocallySavedEntry(
+                locallySavedEntryKey(nameOfUser, year, month, date),
+                entry
+              )
+            }
+            errorOnSave={didCallMutation && errorFromMutation != null}
+            loading={loading}
+          />
+        );
+      }}
+    </Mutation>
+  );
+}
+
+function getEntryForAuthor(author, entries) {
   const entry = entries.filter(({ author: { name } }) => name === author)[0];
 
   if (entry !== undefined) {
@@ -42,72 +172,7 @@ const getEntryForAuthor = (author, entries) => {
   }
 
   return '';
-};
-
-const EditableEntryBody = ({ year, month, date, nameOfUser, entry }) => (
-  <Mutation
-    mutation={gql`
-      mutation SetEntry(
-        $year: Int!
-        $month: Int!
-        $date: Int!
-        $content: String!
-      ) {
-        setEntry(year: $year, month: $month, date: $date, content: $content) {
-          content
-        }
-      }
-    `}
-    refetchQueries={[
-      { query: CALENDAR_GQL_QUERY, variables: { year, month } },
-      { query: ENTRY_GQL_QUERY, variables: { year, month, date } },
-    ]}
-  >
-    {(
-      setEntry,
-      {
-        data: dataFromMutation,
-        called: didCallMutation,
-        error: errorFromMutation,
-        loading,
-      }
-    ) => {
-      if (didCallMutation && errorFromMutation == null) {
-        removeLocallySavedEntry(
-          locallySavedEntryKey(nameOfUser, year, month, date)
-        );
-      }
-
-      return (
-        <EntryForm
-          saveEntry={content =>
-            setEntry({ variables: { year, month, date, content } })
-          }
-          nameOfUser={nameOfUser}
-          entry={
-            (dataFromMutation &&
-              dataFromMutation.setEntry &&
-              dataFromMutation.setEntry.content) ||
-            entry
-          }
-          getLocallySavedEntry={() =>
-            getLocallySavedEntry(
-              locallySavedEntryKey(nameOfUser, year, month, date)
-            )
-          }
-          locallySaveEntry={entry =>
-            setLocallySavedEntry(
-              locallySavedEntryKey(nameOfUser, year, month, date),
-              entry
-            )
-          }
-          errorOnSave={didCallMutation && errorFromMutation != null}
-          loading={loading}
-        />
-      );
-    }}
-  </Mutation>
-);
+}
 
 function getLocallySavedEntry(key) {
   return localStorage.getItem(key);
@@ -124,63 +189,4 @@ function removeLocallySavedEntry(key) {
 function locallySavedEntryKey(user, year, month, date) {
   return `${user}${year}${month}${date}`;
 }
-
-const EntryContainer = ({ year, month, date, author: requestedAuthor }) => (
-  <Query
-    query={ENTRY_GQL_QUERY}
-    variables={{ year, month, date }}
-    fetchPolicy="cache-and-network"
-  >
-    {({ data: dataFromQuery, loading: loadingQuery }) => (
-      <Entry
-        body={
-          loadingQuery ? (
-            <EntryBody nameOfUser={''} entry={''} loading={loadingQuery} />
-          ) : dataFromQuery.me.name.toLowerCase() ===
-          requestedAuthor.toLowerCase() ? (
-            <EditableEntryBody
-              year={year}
-              month={month}
-              date={date}
-              nameOfUser={
-                (dataFromQuery && dataFromQuery.me && dataFromQuery.me.name) ||
-                ''
-              }
-              entry={getEntryForAuthor(
-                requestedAuthor,
-                (dataFromQuery && dataFromQuery.entries) || []
-              )}
-            />
-          ) : (
-            <EntryBody
-              nameOfUser={
-                (dataFromQuery &&
-                  dataFromQuery.myCouple &&
-                  dataFromQuery.myCouple.partner.name) ||
-                ''
-              }
-              entry={getEntryForAuthor(
-                requestedAuthor,
-                (dataFromQuery && dataFromQuery.entries) || []
-              )}
-              loading={loadingQuery}
-            />
-          )
-        }
-        year={year}
-        month={month}
-        date={date}
-        nameOfPartner={
-          (dataFromQuery && dataFromQuery.myCouple) !== undefined
-            ? dataFromQuery.myCouple.me.name.toLowerCase() ===
-              requestedAuthor.toLowerCase()
-              ? dataFromQuery.myCouple.partner.name
-              : dataFromQuery.myCouple.me.name
-            : ''
-        }
-      />
-    )}
-  </Query>
-);
-
 export default EntryContainer;
